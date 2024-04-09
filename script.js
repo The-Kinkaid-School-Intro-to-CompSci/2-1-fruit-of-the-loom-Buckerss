fdd5530c36de2433ab80335f7639bb682d596e43
Abby Woo <abby.woo@Abbys-MacBook.local>
2024-04-09 14:48:02 -0500

    Added code to make cards up to try it outs

--- script.js
+++ script.js
@@ -13,9 +13,38 @@ function clearCards() {
     }
 }
 
+function addFruitCard(fruit) {
+
+    let newFruitCard = document.createElement('div');
+    // newFruitCard.textContent = apple.name;
+
+    let fruitHeading = document.createElement('h3');
+    fruitHeading.textContent = fruit.name;
+    newFruitCard.appendChild(fruitHeading);
+    let fruitColor = document.createElement('p');
+    fruitColor.textContent = `Color: ${fruit.color}`;
+    fruitColor.style.color = fruit.color;
+    newFruitCard.appendChild(fruitColor);
+
+    let fruitContainer = document.querySelector('#cards');
+    newFruitCard.classList.add('fruitCard');
+    fruitContainer.appendChild(newFruitCard);
+}
+
 function runProgram() {
     console.log("Program is running");
 
+    let apple = fruitObjects[0];
+
+    // for(let i =0; i < fruitObjects.length; i++) {
+    //     addFruitCard(fruitObjects[i]);
+    // }
+    for (const fruit of fruitObjects){
+        addFruitCard(fruit);
+    }
+    addFruitCard(apple);
+    
+    
 }
 
 document.addEventListener('DOMContentLoaded', runProgram);
